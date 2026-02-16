b315bd1ab75b97f4996fe01aba5b4f7c7e56bb1b
Pierre-Antoine RIGAUDIER <rigaudier.pa@gmail.com>
2026-02-16 18:11:33 +0100

    test debug stripe

--- routes/stripeWebhooks.js
+++ routes/stripeWebhooks.js
@@ -11,7 +11,10 @@ router.post(
   express.raw({ type: "application/json" }),
   async (request, response) => {
     console.log("🔥 WEBHOOK HIT");
-    // console.log("RAW BODY:", req.body.toString());
+    console.log(
+      "Webhook received, first 200 chars:",
+      req.body.toString("utf8", 0, 200),
+    );
     let event = request.body;
     // Only verify the event if you have an endpoint secret defined.
     // Otherwise use the basic event deserialized with JSON.parse
